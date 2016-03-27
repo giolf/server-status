4f80dd86bb87906a16dc84878274632f0b85b3ea
Giovanni Far <giovanni.far@gmail.com>
2016-03-27 23:28:02 +0200

    loop implementation on cpu controller

--- app/controllers/cpu-controller.js
+++ app/controllers/cpu-controller.js
@@ -1,33 +1,60 @@
 define(['chartJS'], function(chart) {
     var cpuChart = angular.module('cpuChart', []);
 
-    cpuChart.controller('cpuController', ['$scope', function ($scope) {
-        var data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            }]
+    cpuChart.controller('cpuController', ['$scope', '$interval', function ($scope, $interval) {
+        var loopHandler = null;
+        var cpuChart = null;
+        var count = 0;
+
+        var setupCpuChart = function () {
+            var data = {
+                labels: [],
+                datasets: [{
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: []
+                }]
+            };
+
+            var options = {
+                responsive: true,
+                maintainAspectRatio: true
+                // scaleOverride : true,
+                // scaleSteps : 10,
+                // scaleStepWidth : 10,
+                // scaleStartValue : 0
+            };
+
+            var CpuCanvas = angular.element(
+                document.querySelector('#cpu-chart')
+            )[0].getContext("2d");
+
+            cpuChart = new Chart(CpuCanvas)
+                .Line(data, options);
         };
-        var options = {
-            responsive: true,
-            maintainAspectRatio: true
-            // scaleOverride : true,
-            // scaleSteps : 10,
-            // scaleStepWidth : 10,
-            // scaleStartValue : 0
+
+        var getCurrentCpuValue = function() {
+
+        };
+
+        var cpuLoop = function() {
+            console.log("get current cpu value");
+        };
+
+        $scope.init = function(start) {
+            if (start) {
+                setupCpuChart();
+                loopHandler = $interval(cpuLoop, 1000);
+            }
         }
 
-        var canvas = angular.element(
-            document.querySelector('#cpu-chart')
-        )[0].getContext("2d");
-
-        var cpuChart = new Chart(canvas).Line(data, options);
+        $scope.stop = function() {
+            $interval.cancel(loopHandler);
+        }
     }]);
 });
