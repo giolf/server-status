12adce126343ad5bc349bd060d4ce095b12e7a47
Giovanni Far <giovanni.far@gmail.com>
2016-03-28 21:21:04 +0200

    request method implementation into the serverRequests service

--- app/controllers/cpu-controller.js
+++ app/controllers/cpu-controller.js
@@ -1,60 +1,66 @@
 define(['chartJS', 'serverRequestsModule'], function(chart) {
     var cpuChart = angular.module('cpuChart', ['serverRequests']);
 
-    cpuChart.controller('cpuController', ['$scope', '$interval', 'serverRequestsService', function ($scope, $interval, serverRequestsService) {
-        var loopHandler = null;
-        var cpuChart = null;
-        var count = 0;
+    cpuChart.controller('cpuController', ['$scope', '$interval', 'serverRequestsService',
+        function ($scope, $interval, serverRequestsService) {
+            var loopHandler = null;
+            var cpuChart = null;
 
-        var setupCpuChart = function () {
-            var data = {
-                labels: [],
-                datasets: [{
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: []
-                }]
+            var setupCpuChart = function () {
+                var data = {
+                    labels: [],
+                    datasets: [{
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.2)",
+                        strokeColor: "rgba(220,220,220,1)",
+                        pointColor: "rgba(220,220,220,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: []
+                    }]
+                };
+
+                var options = {
+                    responsive: true,
+                    maintainAspectRatio: true
+                    // scaleOverride : true,
+                    // scaleSteps : 10,
+                    // scaleStepWidth : 10,
+                    // scaleStartValue : 0
+                };
+
+                var CpuCanvas = angular.element(
+                    document.querySelector('#cpu-chart')
+                )[0].getContext("2d");
+
+                cpuChart = new Chart(CpuCanvas)
+                    .Line(data, options);
             };
 
-            var options = {
-                responsive: true,
-                maintainAspectRatio: true
-                // scaleOverride : true,
-                // scaleSteps : 10,
-                // scaleStepWidth : 10,
-                // scaleStartValue : 0
+            var getCurrentCpuValue = function() {
+
             };
 
-            var CpuCanvas = angular.element(
-                document.querySelector('#cpu-chart')
-            )[0].getContext("2d");
+            var cpuLoop = function() {
+                var promise = serverRequestsService
+                    .request("service=usageCPU");
 
-            cpuChart = new Chart(CpuCanvas)
-                .Line(data, options);
-        };
+                promise.then(function(data) {
+                    console.log(data);
+                }) ;
+            };
 
-        var getCurrentCpuValue = function() {
+            $scope.init = function(start) {
+                if (start) {
+                    setupCpuChart();
+                    loopHandler = $interval(cpuLoop, 1);
+                }
+            }
 
-        };
-
-        var cpuLoop = function() {
-            console.log(serverRequestsService.serverRequest());
-        };
-
-        $scope.init = function(start) {
-            if (start) {
-                setupCpuChart();
-                loopHandler = $interval(cpuLoop, 1000);
+            $scope.stop = function() {
+                $interval.cancel(loopHandler);
             }
         }
-
-        $scope.stop = function() {
-            $interval.cancel(loopHandler);
-        }
-    }]);
+    ]);
 });
--- app/services/server-requests.js
+++ app/services/server-requests.js
@@ -1,9 +1,21 @@
 define([], function () {
     var serverRequests = angular.module('serverRequests', []);
 
-    serverRequests.service('serverRequestsService', function () {
-        this.serverRequest = function (param) {
-            return "this is my new serverRequest method provided by my new service";
+    serverRequests.service('serverRequestsService', ['$http', '$q', function ($http, $q) {
+        this.request = function (param) {
+            var serverAddress = "http://192.168.1.123/status-server/client.php";
+            var deferred = $q.defer();
+            
+            $http.get(serverAddress + '?' + param).then(
+                function success(data) {
+                    deferred.resolve(data);
+                },
+                function error(error) {
+                    deferred.reject(error);
+                }
+            );
+
+            return deferred.promise;
         };
-    });
+    }]);
 });
